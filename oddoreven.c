dc819fa826ef69193782ca4e48a69a746778eb3f
immkarthikkumar <30829202+immkarthikkumar@users.noreply.github.com>
2017-09-12 15:20:55 +0530

    Update oddoreven.c

--- oddoreven.c
+++ oddoreven.c
@@ -5,10 +5,13 @@ int main()
 
     printf("Enter an integer: ");
     scanf("%d", &number);
+    {
     if(number % 2 == 0)
         printf("%d is even.", number);
+    }
     else
+    {
         printf("%d is odd.", number);
-
+    }
     return 0;
 }
