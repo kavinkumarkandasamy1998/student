0e0ad33833a77f7b6f58a46034c6997f6ae9cdeb
immkarthikkumar <30829202+immkarthikkumar@users.noreply.github.com>
2018-01-18 16:42:20 +0530

    Update prime or not.c

--- prime or not.c
+++ prime or not.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 int main(void)
 {
-int count=0,i,value;
+int count=0,a,value;
 printf("Enter the number:");
 scanf("%d",&value);
-for(i=2;i<value;i++)
+for(a=2;a<value;a++)
 {
-if(value%i==0)
+if(value%a==0)
 {
 count++;
 }
