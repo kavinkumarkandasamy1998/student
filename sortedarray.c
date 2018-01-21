1e502fd19a9ec315f46680caeedad1e51cdd3e58
immkarthikkumar <30829202+immkarthikkumar@users.noreply.github.com>
2018-01-21 11:45:06 +0530

    Create sortedarray.c

---
+++ sortedarray.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include<math.h> 
+void main()
+{
+    int array[10];
+    int i, j, a, temp, keynum;
+    int low, mid, high;
+ 
+    printf("Enter the value of num \n");
+    scanf("%d", &a);
+    printf("Enter the elements one by one \n");
+    for (i = 0; i <a; i++)
+    {
+        scanf("%d", &array[i]);
+    }
+    printf("Input array elements \n");
+    for (i = 0; i <a; i++)
+    {
+        printf("%d\n", array[i]);
+    }
+    printf("the sorted list of array");
+    }
+    }
