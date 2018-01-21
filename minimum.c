3ea8ade51d5ada76217f36773ba92773cfa8ca6d
immkarthikkumar <30829202+immkarthikkumar@users.noreply.github.com>
2018-01-21 11:42:13 +0530

    Create minimum.c

---
+++ minimum.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#define MAX_SIZE 100
+int main()
+{
+    int arr[MAX_SIZE];
+    int i, e, sum=0;   
+    printf("Enter size of the array: ");
+    scanf("%d", &e);
+    printf("Enter %d elements in the array: ", e);
+    for(i=0; i<n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    printf("enter the number in reverse');
+    }
+    }
+  
