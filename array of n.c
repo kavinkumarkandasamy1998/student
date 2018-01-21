6a50867976c58dfff691d51981edc78fc7da3056
immkarthikkumar <30829202+immkarthikkumar@users.noreply.github.com>
2018-01-21 12:05:57 +0530

    Create array of n.c

---
+++ array of n.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int a[10],j,b;
+printf("Enter the array size:");
+scanf("%d",&b);
+printf("The array elements are:");
+for(j=0;j<b;j++)
+{
+scanf("%d",&a[j]);
+}
+for(j=0;j<n;j++)
+{
+printf("%d%d\n",a[j],j);
+}
+}
