b84151038c7d7bb87f7defb9cfbcfa9db2ed3a6d
immkarthikkumar <30829202+immkarthikkumar@users.noreply.github.com>
2018-01-25 16:52:08 +0530

    Update print given times.c

--- print given times.c
+++ print given times.c
@@ -3,11 +3,15 @@
 void main()
 {
     int i,k;
+    char a[100];
     printf("number of times to be printed:");
     scanf("%d",&k);
+     printf("enter the char\n");
+    scanf("%s",&a);
     for(i=0;i<k;i++)
     {
-        printf("laptop\n");
+        printf("%s\n",a);
+       
     }
     getch();
 }
