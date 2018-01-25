1e92138a2e6a4b5533547f8fd784c50db289d6d5
immkarthikkumar <30829202+immkarthikkumar@users.noreply.github.com>
2018-01-25 16:48:19 +0530

    Create print given times.c

---
+++ print given times.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int i,k;
+    printf("number of times to be printed:");
+    scanf("%d",&k);
+    for(i=0;i<k;i++)
+    {
+        printf("laptop\n");
+    }
+    getch();
+}
