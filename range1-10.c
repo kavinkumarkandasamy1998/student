6f2253933766b9d0c54883c61247e9c778437750
immkarthikkumar <30829202+immkarthikkumar@users.noreply.github.com>
2018-02-01 08:35:26 +0000

    Create range1-10.c

---
+++ range1-10.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int a;
+printf("enter the number:");
+scanf("%d",&a);
+if(a==1||a==2||a==3||a==4||a==5||a==6||a==7||a==8||a==9)
+{
+printf("yes");
+}
+else
+{
+    printf("no");
+}   
+}
