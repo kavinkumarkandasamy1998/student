f1f06c6c4b622d1f287521317bc0583ddd5ade93
immkarthikkumar <30829202+immkarthikkumar@users.noreply.github.com>
2018-02-01 07:58:54 +0000

    Create concat two strings without using strcat.c

---
+++ concat two strings without using strcat.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+char a[20],b[30];
+printf("enter the first string:");
+scanf("%s",&a);
+printf("enter the second string:");
+scanf("%s",&b);
+printf("%s%s",a,b);
+}
