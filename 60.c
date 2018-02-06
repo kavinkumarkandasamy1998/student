fa87949cf20c38e2dc63e479ad0ee650421baea3
immkarthikkumar <30829202+immkarthikkumar@users.noreply.github.com>
2018-02-06 16:10:49 +0530

    Create 60.c

---
+++ 60.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+    int i,d,a=1,b=1,c;
+    scanf("%d",&d);
+    printf("%d %d ",a,b);
+    for( i =2; i <d; i++)
+    {
+        c=a+b;
+        a=b;
+        b=c;
+        printf("%d ",c);
+    }
+}
